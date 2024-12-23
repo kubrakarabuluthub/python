fe3acbeddfb18b4ed1fad87962cab4efb4bd135f
Kübra <kubra32karabulut@gmail.com>
2024-12-23 11:48:47 +0300

    Add files via upload

---
+++ 241209/ornek1.py
@@ -0,0 +1,6 @@
+Degişken1=False                     #degişkene false degeri atanır
+Degişken2=True                      #degişkene true  degeri atanır 
+Degişken3=5                         #degişken 3e 5 degeri atanır 
+degişken4=6.4                       #degişken 4 e 6 . 4 degeri atanır
+degişken5=Degişken3>degişken4       #degişken5'e degisken3 > degisken4 atanır
+print(degişken5)                    #sonuc
---
+++ 241209/ornek3.py
@@ -0,0 +1,10 @@
+#kullaniciya adini ve yaşini sorunuz
+#ve girilen ad ve yaşi ekrana yazdiriniz
+
+ad = input ("lütfen adınızı girin: ") #metinsel türde değer alir
+print("girilen ad",ad)
+
+#sayısal türde girilecekse int() yada float() fonksiyiyonları kullan
+
+yas = int(input("yaşınızı girin:")) #input ile okuyup int türüne dönüştürdük
+print(yas)
